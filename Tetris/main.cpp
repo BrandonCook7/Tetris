66599502486ed910aa40a6cd12f491c13659240a
mattbal <55065240+mattbal@users.noreply.github.com>
2020-04-29 17:57:27 -0700

    Added basis for displaying blocks
    Got rid of music because it lagged program and didn't loop
    
    Added basic mechanisms for displaying blocks and rotating them.

--- Tetris/main.cpp
+++ Tetris/main.cpp
@@ -1,56 +1,52 @@
 #include <SFML/Audio.hpp>
 #include <SFML/Graphics.hpp>
 
-int grid[20][10] = {0};
-
 typedef struct coordinate {
     int x;
     int y;
 } Coordinate;
 
-// Create block shapes using 4x4 coordinate grid. Starts at {0,0} top left and ends at {3,3} bottom right.
-Coordinate shapes[7][4] = {
-    {{1,0},{1,1},{1,2},{1,3}}, // I
-    {{0,1},{1,1},{0,2},{1,2}}, // O (square)
-    {{1,1},{2,1},{0,2},{1,2}}, // S
-    {{0,1},{1,1},{1,2},{2,2}}, // Z
-    {{1,0},{1,1},{1,2},{2,2}}, // L
-    {{1,0},{1,1},{0,2},{1,2}}, // J
-    {{0,1},{1,1},{2,1},{1,2}}  // T
-};
-
-int main()
-{
+int main() {
+    
     // Create the main window
     sf::RenderWindow window(sf::VideoMode(800, 800), "Tetris");
 
-    // Set the Icon
-//    sf::Image icon;
-//    if (!icon.loadFromFile(resourcePath() + "icon.png")) {
-//        return EXIT_FAILURE;
-//    }
-//    window.setIcon(icon.getSize().x, icon.getSize().y, icon.getPixelsPtr());
+    // Create block shapes using 4x4 coordinate grid. Starts at {0,0} top left and ends at {3,3} bottom right.
+    Coordinate shapes[7][4] = {
+        {{1,0},{1,1},{1,2},{1,3}}, // I
+        {{0,1},{1,1},{0,2},{1,2}}, // O (square)
+        {{1,1},{2,1},{0,2},{1,2}}, // S
+        {{0,1},{1,1},{1,2},{2,2}}, // Z
+        {{1,0},{1,1},{1,2},{2,2}}, // L
+        {{1,0},{1,1},{0,2},{1,2}}, // J
+        {{0,1},{1,1},{2,1},{1,2}}  // T
+    };
 
-    // Load a sprite to display
-    sf::Texture block;
-    if (!block.loadFromFile("images/block.jpg")) {
+    Coordinate block[4]; // used to hold the 4 coordinates of a block
+
+    int grid[20][10] = {0};
+
+    // Load block texture and sprites
+    sf::Texture blockTexture;
+    if (!blockTexture.loadFromFile("images/block.jpg")) {
         return EXIT_FAILURE;
     }
-    sf::Sprite sprite(block);
-    sprite.setColor(sf::Color(0,255,0));
     
-    sf::Sprite red(block);
+    sf::Sprite green(blockTexture);
+    green.setColor(sf::Color(0,255,0));
+    
+    sf::Sprite red(blockTexture);
     red.setColor(sf::Color(255,0,0));
     
-    
-    sf::Texture background;
-    if (!background.loadFromFile("images/bg.jpg")) {
+    // Load grid texture and sprite
+    sf::Texture gridTexture;
+    if (!gridTexture.loadFromFile("images/grid.jpg")) {
         return EXIT_FAILURE;
     }
-    sf::Sprite bg(background);
+    sf::Sprite gridSprite(gridTexture);
     
     
-    // Create a graphical text to display
+    // Create text to display
     sf::Font font;
     if (!font.loadFromFile("Roboto-Regular.ttf")) {
         return EXIT_FAILURE;
@@ -79,18 +75,23 @@ int main()
 
         // Clear screen
         window.clear(sf::Color::White);
+        
+        // Test code. Delete later. Sets block to be I
+        for(int i = 0; i < 4; i++) {
+            block[i].x = shapes[0][i].x;
+            block[i].y = shapes[0][i].y;
+        }
+        
+        window.draw(gridSprite);
+        
+        // Test code. Delete later. Sets red sprite to be each block in I and draws it to screen.
+        for(int i = 0; i < 4; i++) {
+            red.setPosition(10 + block[i].x * 32, 10 + block[i].y * 32); // multiply by 32 so the block's dont overlap. (Each block is 32x32). Add 10 px to both values so they are within the grid. Grid image has 10px border
+            window.draw(red);
+        }
+        
 
-        // Draw the sprites
-        
-        sprite.setPosition(42, 42); //board is 10 px in on each side
-        red.setPosition(10,10);
-        
-        
-        window.draw(bg);
-        window.draw(sprite);
-        window.draw(red);
-
-        // Draw the string
+        // Draw the text
         window.draw(text);
 
         // Update the window
