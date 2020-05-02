107bebfd2811f4b1a366958bf9543d179e5bb82c
mattbal <55065240+mattbal@users.noreply.github.com>
2020-05-01 19:48:07 -0700

    made it so block gets stuck at bottom

--- Tetris/tetris.cpp
+++ Tetris/tetris.cpp
@@ -2,52 +2,76 @@
 
 tetris::tetris()
 {
+
     srand(time(0));
-    RenderWindow window(VideoMode(320, 480), "Tetris In C++"); // creates window
+    RenderWindow window(VideoMode(800, 800), "Tetris In C++"); // creates window
     float timer = 0; // timer
     float delay = 0.3; // delay in milliseconds
     Clock clock; // clock
-
+    
     hasSpawned = 0;
     int isSquare = 0;
+    
     // Load textures and sprites
+    
     // Blocks.jpg has 7 blocks of different color. Tried using just a grey scale image and setColor, but it made blocks too dark. This way allows us to change color by changing what part of the image a sprite uses
+    
     Texture blockTexture;
     if (!blockTexture.loadFromFile("images/blocks.jpg")) {
-        cout << "Unable to open images/blocks.jpg" << endl;
+        return EXIT_FAILURE;
     }
     blockTexture.setSmooth(true);
     Sprite sprite(blockTexture);
-
+    
     Texture gridTexture;
     if (!gridTexture.loadFromFile("images/grid.jpg")) {
-        cout << "Unable to open images/grid.jpg" << endl;
+        return EXIT_FAILURE;
     }
     Sprite gridSprite(gridTexture);
+    
+    
+    /* Text code. Move somewhere later
+     
+    Font font;
+    if (!font.loadFromFile("Roboto-Regular.ttf")) {
+        return EXIT_FAILURE;
+    }
+    Text text("Hello SFML", font, 50);
+    text.setFillColor(Color::Black);
+    text.setPosition(400, 32);
+
+     
+     */
+    
+    
 
     while (window.isOpen())
     {
         float time = clock.getElapsedTime().asSeconds();
         clock.restart();
         timer += time;
+        
         // Process events
         Event event;
         while (window.pollEvent(event)) {
-
+            
             // Close window: exit
             if (event.type == Event::Closed) {
                 window.close();
             }
-
+            
             if (event.type == Event::KeyPressed) {
-                if (event.key.code == Keyboard::Escape) window.close();
-                if (event.key.code == Keyboard::Left) dx = -1;
-                if (event.key.code == Keyboard::Right) dx = 1;
-                if (event.key.code == Keyboard::Up) needsRotation = true;
+                if(event.key.code == Keyboard::Escape) window.close();
+                if(event.key.code == Keyboard::Left) dx = -1;
+                if(event.key.code == Keyboard::Right) dx = 1;
+                if(event.key.code == Keyboard::Up) needsRotation = true;
             }
         }
+        
         move();
-        if (needsRotation && !isSquare) rotate();
+        if(needsRotation && !isSquare) rotate();
+        //down
+        spawn(sprite, isSquare);
         if (timer > delay)
         {
             for (int i = 0; i < 4; i++)
@@ -57,32 +81,51 @@ tetris::tetris()
             }
             if (!checkBounds())
             {
-                spawn(sprite, isSquare);
+                for(int i = 0; i < 4; i++) {
+                    current[i].y = prev[i].y;
+                }
+                // update grid
             }
             timer = 0;
         }
+        
+        // check line
+        
         dx = 0;
         needsRotation = false;
+
         // Clear screen
         window.clear(Color::White);
         window.draw(gridSprite);
+        
         // Draw the sprite in correct spot
-        for (int i = 0; i < 4; i++) {
-            sprite.setPosition(current[i].x * 32 + 10 + 96, current[i].y * 32 + 10); // multiply by 32 so the block's dont overlap. (Each block is 32x32). Add 10 px to both values so they are within the grid. Grid image has 10px border
+        for(int i = 0; i < 4; i++) {
+            sprite.setPosition(current[i].x * 32 + 10, current[i].y * 32 + 10); // multiply by 32 so the block's dont overlap. (Each block is 32x32). Add 10 px to both values so they are within the grid. Grid image has 10px border
             window.draw(sprite);
         }
+        
+        // check if block cleared level
+        
+        // if block cleared level, increase score, reset hasSpawned to 0 and isSquare to 0.
+        
         // add display score here
+        
+
         window.display(); // Update the window
+        
     }
+    
 }
 
 bool tetris::checkBounds()
 {
-    for (int i = 0; i < 4; i++)
+    for (int i = 0; i < 4; i++) {
         if (current[i].x < 0 || current[i].x >= 10 || current[i].y >= 20)
             return false;
-        else if (grid[current[i].y][current[i].x])
+        else if (grid[current[i].y][current[i].x] != 0)
             return false;
+    }
+    
     return true;
 }
 
@@ -108,8 +151,8 @@ void tetris::checkLines()
 void tetris::rotate() {
     Coordinate temp = current[1];
     int x = 0, y = 0;
-
-    for (int i = 0; i < 4; i++) {
+    
+    for(int i = 0; i < 4; i++) {
         x = current[i].y - temp.y;
         y = current[i].x - temp.x;
         current[i].x = temp.x - x;
@@ -118,31 +161,34 @@ void tetris::rotate() {
 }
 
 void tetris::move() {
-    for (int i = 0; i < 4; i++) {
+    for(int i = 0; i < 4; i++) {
         prev[i].x = current[i].x; // used to restore block's position if it goes out of bounds
         current[i].x += dx;
     }
-
-    if (!checkBounds()) { // went out of bounds
-        for (int i = 0; i < 4; i++) {
+    
+    if(!checkBounds()) { // went out of bounds
+        for(int i = 0; i < 4; i++) {
             current[i].x = prev[i].x;
         }
     }
 }
 
-void tetris::spawn(Sprite& sprite, int &isSquare) {
-    if (hasSpawned == 0) { // check if a block has been spawned already
+void tetris::spawn(Sprite &sprite, int &isSquare) {
+    
+    if(hasSpawned == 0) { // check if a block has been spawned already
         int randNum = rand() % 7;
-        if (randNum == 1) isSquare = 1;
-        sprite.setTextureRect(IntRect(randNum * 32, 0, 32, 32)); // set color of sprite
-        for (int i = 0; i < 4; i++) {
+        if(randNum == 1) isSquare = 1;
+        sprite.setTextureRect(IntRect(randNum * 32,0,32,32)); // set color of sprite
+        for(int i = 0; i < 4; i++) {
             current[i].x = shapes[randNum][i].x;
             current[i].y = shapes[randNum][i].y;
         }
+        
         hasSpawned = 1;
     }
 }
 
 bool tetris::checkWin() {
+    
     return true;
 }
--- Tetris/tetris.h
+++ Tetris/tetris.h
@@ -3,7 +3,7 @@
 
 #include "game.h"
 
-class tetris : public game
+class tetris: public game
 {
 private:
     int score;
@@ -12,8 +12,8 @@ private:
     float timer; // timer
     float delay; // delay in milliseconds
     Clock clock; // clock
-
-    int grid[20][10]; // playing field
+    
+    int grid[20][10] = {0}; // playing field
     Coordinate shapes[7][4] = // different pieces
     {
         {{1,0},{1,1},{1,2},{1,3}}, // I
@@ -25,7 +25,6 @@ private:
         {{0,1},{1,1},{2,1},{1,2}}  // T
     };
     Coordinate current[4], prev[4]; // keeps track of block's current position and previous position
-    Coordinate block[4]; // holds the 4 coordinates of a block
 public:
     tetris();
     bool checkWin();
@@ -33,8 +32,8 @@ public:
     void checkLines();
     void rotate();
     void move();
-    void spawn(Sprite& sprite, int& isSquare);
+    void spawn(Sprite &sprite, int &isSquare);
     void displayScore();
 };
 
-#endif 
+#endif /* tetris_hpp */
